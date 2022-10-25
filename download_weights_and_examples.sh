53893c08349bff8d98be20dc2d773d77e6fa2d9a
Moliner Eloi <molinee2@login3.triton.aalto.fi>
2022-10-25 15:16:02 +0300

    bug_download_examples

--- download_weights_and_examples.sh
+++ download_weights_and_examples.sh
@@ -6,4 +6,4 @@ mkdir experiments/cqt
 mv cqt_weights.pt  experiments/cqt/
 
 wget https://github.com/eloimoliner/CQTdiff/releases/download/weights_and_examples/examples.zip
-unzip examples.zip .
+unzip examples.zip 
